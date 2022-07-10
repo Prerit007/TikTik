31fdaa20432af43eecca8c8577ba4361bf2397ff
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-10 17:03:15 +0530

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {useState, useEffect} from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setisSSR] = useState(true);
@@ -12,10 +14,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
   return (
     <div>
-      NavBar
+      <Navbar/>
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          SideBar
+          <Sidebar/>
         </div>
         <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
         <Component {...pageProps} />
