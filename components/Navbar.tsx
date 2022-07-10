98d6c2d5687a5047c74238104f5a10504ddc4f2b
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-10 20:17:51 +0530

    commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, GoogleLogout  } from 'react-google-login';
 
 const Navbar = () => {
   return (
