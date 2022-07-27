58e987d8948d2551535c7fe248f70a450297c8c6
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-27 20:40:17 +0530

    commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
+
+import SuggestedAccounts from './SuggestedAccounts';
+import Discover from './Discover';
+import Footer from './Footer';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
