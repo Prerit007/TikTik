38aba223f9f5b67fed13bafbef4f732547341dd7
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2022-07-28 13:24:15 +0530

    commit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
@@ -10,10 +10,13 @@ import Discover from './Discover';
 import Footer from './Footer';
 
 const Sidebar = () => {
+  const [showSidebar, setShowSidebar] = useState<Boolean>(true);
+  const { pathname } = useRouter();
+
   return (
     <div>
-      <div>
-        
+      <div className="block md:hidden">
+        Sidebar
       </div>
     </div>
   )
